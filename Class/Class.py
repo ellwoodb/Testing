7702d3e4ed5087c5b709555848c69ef348c90c02
ellwoodb <56759474+matthew230506@users.noreply.github.com>
2020-11-08 11:47:43 +0100

    Class testing; .gitignore ignore __pycache__

--- Class/Class.py
+++ Class/Class.py
@@ -1,7 +1,7 @@
-# class TestClass:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-#
-#     def Person(self):
-#         print("Name: ", self.name, "\nAge: ", self.age)
+class TestClass:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def person(self):
+        print("Name: ", self.name, "\nAge: ", self.age)
--- Class/Test.py
+++ Class/Test.py
@@ -1,4 +1,8 @@
-from test import TestClass
+from Class import TestClass
+from Folder.Class2 import TestClass2
 
 TC = TestClass("Niels", "11")
-TC.Person()
+TC.person()
+
+TC2 = TestClass2("Wurst", "5€")
+TC2.buy()
